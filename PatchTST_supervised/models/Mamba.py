d3cf69f229e2521e17d9fcaeeb2336a1b86eff1b
jylli <li_jiyanglin@163.com>
2024-06-26 18:17:49 +0800

    新增model：Mamba，还没有把模型的框架写好

---
+++ PatchTST_supervised/models/Mamba.py
@@ -0,0 +1,14 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+import numpy as np
+
+
+class Model(nn.Module):
+    def __init__(self, configs):
+        super(Model, self).__init__()
+
+
+    def forward(self):
+        pass
