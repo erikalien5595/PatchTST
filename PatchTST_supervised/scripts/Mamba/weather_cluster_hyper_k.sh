d1b503bf4c7d6f399ea9b4cca84ed63101e017ba
jylli <li_jiyanglin@163.com>
2025-04-09 18:20:07 +0800

    新增weather聚类超参敏感性实验的.sh脚本

---
+++ PatchTST_supervised/scripts/Mamba/weather_cluster_hyper_k.sh
@@ -0,0 +1,55 @@
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+gpu=3
+
+if [ ! -d "./logs" ]; then
+    mkdir ./logs
+fi
+
+if [ ! -d "./logs/LongForecasting" ]; then
+    mkdir ./logs/LongForecasting
+fi
+seq_len=96
+model_name=Mamba
+
+root_path_name=./dataset/weather/
+data_path_name=weather.csv
+model_id_name=Weather # 如果是聚类后的模型，model_id_name后面再加上_cluster
+data_name=custom
+
+random_seed=2024
+for n_clusters in 3 5 7 9 15 20 21
+do
+for pred_len in 96
+do
+python -u run_longExp.py \
+  --random_seed $random_seed \
+  --is_training 1 \
+  --root_path $root_path_name \
+  --data_path $data_path_name \
+  --model_id $model_id_name'_'$seq_len'_'$pred_len \
+  --model $model_name \
+  --data $data_name \
+  --features M \
+  --is_cluster 1 \
+  --n_clusters $n_clusters \
+  --revin 1 \
+  --seq_len $seq_len \
+  --pred_len $pred_len \
+  --enc_in 21 \
+  --e_layers 2 \
+  --n_heads 16 \
+  --d_model 512 \
+  --d_state 16 \
+  --d_ff 256 \
+  --is_flip 1 \
+  --dropout 0.1 \
+  --des '聚类个数k敏感性_全CD' \
+  --use_wandb True \
+  --train_epochs 10 \
+  --patience 5\
+  --lradj '5'\
+  --gpu ${gpu} \
+  --itr 1 --batch_size 128 --learning_rate 0.0001 #>logs/LongForecasting/$model_name'_'$model_id_name'_'$seq_len'_'$pred_len.log
+done
+done
+
