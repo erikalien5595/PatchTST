c460bb2d78509542b4ac296dbd363da68b6e99a8
jylli <li_jiyanglin@163.com>
2025-04-09 18:36:25 +0800

    新增weather聚类tau敏感性实验的.sh脚本

--- PatchTST_supervised/scripts/Mamba/weathter_cluster_tau.sh
+++ PatchTST_supervised/scripts/Mamba/weathter_cluster_tau.sh
@@ -1,5 +1,5 @@
 export CUDA_VISIBLE_DEVICES=0,1,2,3
-gpu=0
+gpu=1
 
 if [ ! -d "./logs" ]; then
     mkdir ./logs
@@ -17,7 +17,7 @@ model_id_name=Weather # 如果是聚类后的模型，model_id_name后面再加�
 data_name=custom
 
 random_seed=2024
-for corr_threshold in 0.2 0.4 0.6 0.8
+for corr_threshold in 0.1 0.3 0.5 0.7 0.9
 do
 for pred_len in 96
 do
